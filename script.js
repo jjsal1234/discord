763783e0449e43287a3b4dd53473cec49934ac68
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2024-03-11 16:11:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let countdownInterval;
 
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 100); // Generates a random number between 0 and 99
+    return Math.floor(Math.random() * 1000001); // Generates a random number between 0 and 1 million
 }
 
 function sendToDiscord(randomNumber) {
