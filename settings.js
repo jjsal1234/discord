8b19918f619a842b1e0147451efbfe9a11910ddc
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2024-03-11 20:41:17 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,8 @@
-function openSettings() {
-    const settingsPopup = document.getElementById("settingsPopup");
-    settingsPopup.style.display = "block";
-}
-
-function closeSettings() {
-    const settingsPopup = document.getElementById("settingsPopup");
-    settingsPopup.style.display = "none";
-}
-
-function toggleAppearanceSettings() {
-    // Add your appearance settings functionality here
-    alert("Appearance settings will be available soon!");
-}
-
-function toggleSoundSettings() {
-    // Add your sound settings functionality here
-    alert("Sound settings will be available soon!");
+function toggleSettings() {
+    var settingsPopup = document.getElementById("settingsPopup");
+    if (settingsPopup.style.display === "block") {
+        settingsPopup.style.display = "none";
+    } else {
+        settingsPopup.style.display = "block";
+    }
 }
