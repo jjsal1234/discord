fc106380544ae0c9242ab63667621aa419ffcc39
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2024-03-11 21:27:12 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,17 @@
 function toggleSettings() {
     var settingsPopup = document.getElementById("settingsPopup");
-    if (settingsPopup.style.display === "block") {
+    if (settingsPopup.style.display === "block" || settingsPopup.style.display === "") {
         settingsPopup.style.display = "none";
     } else {
         settingsPopup.style.display = "block";
     }
+}
+
+function showCategory(categoryName) {
+    var categories = document.querySelectorAll(".category");
+    categories.forEach(function(category) {
+        category.classList.remove("active");
+    });
+    var category = document.getElementById(categoryName);
+    category.classList.add("active");
 }
