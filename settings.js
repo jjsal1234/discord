5f6a2aaa9cdb56c2f16a80611e4fbf8b92e3e871
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2024-03-11 19:15:43 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,10 @@
-function openSettings() {
-    // Add your settings functionality here
-    alert("Settings will be available soon!");
+function updateSettings() {
+    const intervalInput = document.getElementById("intervalInput");
+    const generateInterval = parseInt(intervalInput.value, 10);
+    const themeSelect = document.getElementById("themeSelect");
+    const selectedTheme = themeSelect.value;
+    
+    // Update settings
+    localStorage.setItem("generateInterval", generateInterval);
+    localStorage.setItem("theme", selectedTheme);
 }
